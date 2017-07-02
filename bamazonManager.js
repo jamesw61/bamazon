a7e05b54906ce05b895f0330a9d0973e79c89883
James Wick <jmwick61@gmail.com>
2017-07-02 05:06:31 -0700

    phase 2 works

--- bamazonManager.js
+++ bamazonManager.js
@@ -1,51 +1,60 @@
 'use strict';
 var mysql = require("mysql");
 var inquirer = require('inquirer');
-// var connection = require('./bamazonCustomer'); //????
-
-// connection.connect(function(err) {
-//     if (err) throw err;
-//     console.log('connected as id ' + connection.threadId);
-// });
-
-inquirer.prompt([{
-    type: 'rawlist',
-    name: 'menuPick',
-    message: 'What do you want to do?',
-    choices: [
-        'View Products for Sale',
-        'View Low Inventory',
-        new inquirer.Separator(),
-        'Add to Inventory',
-        'Add New Product'
-    ]
-}]).then(function(answer) {
-    var pick3 = JSON.stringify(answer, null, '  ');
-    var pick2 = JSON.parse(pick3);
-    var pick = pick2.menuPick;
-    console.log(pick);
-    switch (pick) {
-        case 'View Products for Sale':
-            console.log('a');
-            var query = 'SELECT * FROM products';
-            view(query);
-            break;
-        case 'View Low Inventory':
-            console.log('b');
-			var query2 = 'SELECT * FROM products WHERE stock_quantity < 5';
-			view(query2);
-            break;
-        case 'Add to Inventory':
-            console.log('c');
-
-            break;
-        case 'Add New Product':
-            console.log('d');
-            break;
-    }
+// var connection = require('./bamazonCustomer');
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "steel61sql",
+    database: 'bamazon'
 });
 
-var view = function(queryString) {	
+
+connection.connect(function(err) {
+    if (err) throw err;
+    console.log('connected as id ' + connection.threadId);
+    start();
+});
+
+function start() {
+    inquirer.prompt([{
+        type: 'rawlist',
+        name: 'menuPick',
+        message: 'What do you want to do?',
+        choices: [
+            'View Products for Sale',
+            'View Low Inventory',
+            new inquirer.Separator(),
+            'Add to Inventory',
+            'Add New Product'
+        ]
+    }]).then(function(answer) {
+        var pick3 = JSON.stringify(answer, null, '  ');
+        var pick2 = JSON.parse(pick3);
+        var pick = pick2.menuPick;
+        console.log(pick);
+        switch (pick) {
+            case 'View Products for Sale':
+                var query = 'SELECT * FROM products';
+                view(query);
+                break;
+            case 'View Low Inventory':
+                var query2 = 'SELECT * FROM products WHERE stock_quantity < 5';
+                view(query2);
+                break;
+            case 'Add to Inventory':
+                addInventory();
+                break;
+            case 'Add New Product':
+                console.log('d');
+                addProduct();
+                break;
+        }
+    });
+}
+
+var view = function(queryString) {
     connection.query(queryString, function(err, res) {
         if (err) throw err;
         console.log("\nId | Product | Department | Price | Quantity Available");
@@ -55,5 +64,95 @@ var view = function(queryString) {
         }
     });
 }
+var choiceArray = [];
+var addInventory = function() {
+    var query = 'SELECT * FROM products';
+    var name = "";
+    var stock = 0;
+    connection.query(query, function(err, res) {
+        if (err) throw err;
+        for (let i = 0; i < res.length; i++) {
+            choiceArray.push(res[i].name);
+        }
+        console.log(choiceArray);
+        inquirer.prompt([{
+                type: 'rawlist',
+                name: 'product',
+                message: "What product would you like to replenish?",
+                choices: choiceArray
+            }, {
+                type: 'input',
+                name: 'amount',
+                message: 'How many would you like to add?'
+            }])
+            .then(function(answer) {
+                var x = JSON.stringify(answer, null, 2);
+                var y = JSON.parse(x);
+                var amount = parseInt(y.amount);
+                name = y.product;
+                console.log(name + "    " + amount);
+                for (let j = 0; j < res.length; j++) {
+                    if (res[j].name === name) {
+                        stock = res[j].stock_quantity + amount;
+                        updateRow(stock, name);
+                        return;
+                    }
+                }
+            });
+    });
+}
+
+function updateRow(stock, name) {
+    connection.query(
+        "UPDATE products SET ? WHERE ?", [{
+            stock_quantity: stock
+        }, {
+            name: name
+        }],
+        function(err, res) {
+            if (err) throw err;
+        });
+}
+
+function addProduct() {
+    inquirer.prompt([{
+            type: 'input',
+            name: 'name',
+            message: 'What product would you like to add?'
+        }, {
+            type: 'input',
+            name: 'dept',
+            message: 'What department?'
+        }, {
+            type: 'input',
+            name: 'price',
+            message: 'What is the selling price?'
+        }, {
+            type: 'input',
+            name: 'stock_quantity',
+            message: 'How many will be stocked?'
+        }])
+        .then(function(answer) {
+            var x = JSON.stringify(answer, null, 2);
+            var y = JSON.parse(x);
+            console.log(y);
+            var name = y.name;
+            var dept = y.dept;
+            var price = parseFloat(y.price);
+            var quantity = parseInt(y.stock_quantity);
+            createRow(name, dept, price, quantity);
+        });
+}
 
 
+function createRow(name, dept, price, quantity){
+    connection.query("INSERT INTO products SET ?",
+    {
+        name: name,
+        dept: dept,
+        price: price,
+        stock_quantity: quantity
+    }, function(err,res){
+        if(err) throw err;        
+    });
+}
